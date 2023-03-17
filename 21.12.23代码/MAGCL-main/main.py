d31cf525f39340091ed324fa7f0afa6c8d0c4e8a
Linnkid_CHEN <56332312+LinnkidChen@users.noreply.github.com>
2023-03-17 22:57:59 +0800

    1

--- 21.12.23代码/MAGCL-main/main.py
+++ 21.12.23代码/MAGCL-main/main.py
@@ -1,5 +1,6 @@
 import argparse
 import os.path as osp
+import os
 import random
 import nni
 import yaml
@@ -46,24 +47,33 @@ from utils import (
 
 
 def process_data():
+    # 用heco的baseline
+    # train_20 train类别每个label有20个节点
+
     # feat_path = '/nfs_baoding_ai/xumeng/run_emb/HeCo/data/acm/p_feat.npz'
-    feat_path = "../data/acm/p_feat.npz"
+    # feat_path = "../data/acm/p_feat.npz"
+    feat_path = "./data/acm/p_feat.npz"  # paper feature (x)
     # path_1 = '/nfs_baoding_ai/xumeng/run_emb/HeCo/data/acm/pap_idx.npy'
-    path_1 = "../data/acm/pap_idx.npy"
+    # path_1 = "../data/acm/pap_idx.npy"
+    path_1 = "./data/acm/pap_idx.npy"  # metapath pap 临接
     # path_2 = '/nfs_baoding_ai/xumeng/run_emb/HeCo/data/acm/psp_idx.npy'
-    path_2 = "../data/acm/psp_idx.npy"
+    path_2 = "./data/acm/psp_idx.npy"  # metapath psp 临接
+    # path_2 = "../data/acm/psp_idx.npy"
     # path = '/nfs_baoding_ai/xumeng/run_emb/HeCo/data/acm/'
-    path = "../data/acm/"
-    pap = torch.from_numpy(np.load(path_1))
-    psp = torch.from_numpy(np.load(path_2))
+    path = "./data/acm/"
+    # path = "../data/acm/"
+    pap = torch.from_numpy(np.load(path_1))  # paper author paper 将pa ap 两条边合成一条片p-p
+    psp = torch.from_numpy(np.load(path_2))  # paper subject paper
+    # 目的是预测paper的类别
     features = sp.load_npz(feat_path)
     features = torch.from_numpy(features.todense())
-    label = np.load(path + "labels.npy").astype("int32")
+    label = np.load(path + "labels.npy").astype("int32")  # Y
     train = torch.from_numpy(np.load(path + "train_" + "20" + ".npy"))
     test = torch.from_numpy(np.load(path + "test_" + "20" + ".npy"))
     val = torch.from_numpy(np.load(path + "val_" + "20" + ".npy"))
 
     return [pap, psp], features, label, train, val, test
+    # edge_list, features, label, train_idx, val, test_idx
 
 
 def train():
@@ -71,19 +81,21 @@ def train():
     optimizer.zero_grad()
     # edge_index_1 = dropout_adj(data.edge_index, p=drop_edge_rate_1)[0]
     # edge_index_2 = dropout_adj(data.edge_index, p=drop_edge_rate_2)[0] #adjacency with edge droprate 2
-    edge_index_1 = edge_list[0].to(device)
+    edge_index_1 = edge_list[0].to(device)  # 邻接矩阵 扰动
     edge_index_2 = edge_list[1].to(device)
     # edge_index_1 = dropout_adj(edge_index_1, p=drop_edge_rate_1)[0]
     # edge_index_2 = dropout_adj(edge_index_2, p=drop_edge_rate_2)[0]
     # x_1 = drop_feature(features, drop_feature_rate_1)#3
     # x_2 = drop_feature(features, drop_feature_rate_2)#4
-    x_1 = features
+    x_1 = features  # 特征矩阵 没有扰动
     x_2 = features
     m = random.randint(2, 6)
-    z1 = model(x_1, edge_index_1, [2, 4])
+    z1 = model(
+        x_1, edge_index_1, [2, 4]
+    )  # a(axW1)W2, --> a^4(a^2xW1)W2 ->GCN(encoder)  W1,W2两层的参数 A=邻接矩阵 X=特征矩阵
     z2 = model(x_2, edge_index_2, [8, 4])
 
-    loss = model.loss(
+    loss = model.loss(  # GRACE infoNce
         z1,
         z2,
         batch_size=64
@@ -117,22 +129,24 @@ def test(final=False):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--device", type=str, default="cuda:5")
-    parser.add_argument("--dataset", type=str, default="Cora")
+    # parser.add_argument("--device", type=str, default="cuda:5")
+    parser.add_argument("--device", type=str, default="cpu")
+    parser.add_argument("--dataset", type=str, default="ACM")
     parser.add_argument("--config", type=str, default="param.yaml")
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--verbose", type=str, default="train,eval,final")
     parser.add_argument("--save_split", type=str, nargs="?")
     parser.add_argument("--load_split", type=str, nargs="?")
     args = parser.parse_args()
-
-    config = yaml.load(open(args.config), Loader=SafeLoader)[args.dataset]
+    print(os.getcwd())
+    # config = yaml.load(open(args.config), Loader=SafeLoader)[args.dataset]
+    config = yaml.load(open("./MAGCL-main/param.yaml"), Loader=SafeLoader)[args.dataset]
 
     torch.manual_seed(args.seed)
     random.seed(0)
     np.random.seed(args.seed)
     use_nni = args.config == "nni"
-    learning_rate = config["learning_rate"]
+    learning_rate = config["learning_rate"]  # para2
     num_hidden = config["num_hidden"]
     num_proj_hidden = config["num_proj_hidden"]
     activation = config["activation"]
@@ -145,14 +159,23 @@ if __name__ == "__main__":
     drop_feature_rate_2 = config["drop_feature_rate_2"]
     drop_scheme = config["drop_scheme"]
     tau = config["tau"]
-    num_epochs = 10
+    num_epochs = 10  # para1
     weight_decay = config["weight_decay"]
     rand_layers = config["rand_layers"]
 
     device = torch.device(args.device)
 
     edge_list, features, label, train_idx, val, test_idx = process_data()
+    # edge_list[0].shape=[57853,2]
+    # edge_list[1].shape= [4338213,2]
+    # features.shape=[4019,1902]
+    # label.shape=(4019)
+    # train_idx.shape=([60])
+    # val.shape=([1000])
+    # test_idx.shape=([1000])
     edge_list = [idx.t().to(device) for idx in edge_list]
+    # t()=transpose()
+
     features = features.float().to(device)
     split = {"train": train_idx, "val": val, "test": test_idx}
     # if args.dataset == 'Cora' or args.dataset == 'CiteSeer' or  args.dataset == 'PubMed': split = (data.train_mask, data.val_mask, data.test_mask)
@@ -161,7 +184,9 @@ if __name__ == "__main__":
         1902, num_hidden, get_activation(activation), base_model=NewGConv, k=num_layers
     ).to(device)
     adj = 0
-    model = NewGRACE(encoder, adj, num_hidden, num_proj_hidden, tau).to(device)
+    model = NewGRACE(encoder, adj, num_hidden, num_proj_hidden, tau).to(
+        device
+    )  # 对比学习的模型 loss在这个里边
 
     optimizer = torch.optim.Adam(
         model.parameters(), lr=learning_rate, weight_decay=weight_decay
--- 21.12.23代码/MAGCL-main/pGRACE/model.py
+++ 21.12.23代码/MAGCL-main/pGRACE/model.py
@@ -37,7 +37,7 @@ from torch_geometric.typing import (
 )
 from torch_geometric.utils import add_self_loops, remove_self_loops, softmax
 
-#from ..inits import glorot, zeros
+# from ..inits import glorot, zeros
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -46,7 +46,15 @@ from torch_geometric.nn import GCNConv
 
 
 class Encoder(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, activation, base_model=GCNConv, k: int = 2, skip=False):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        activation,
+        base_model=GCNConv,
+        k: int = 2,
+        skip=False,
+    ):
         super(Encoder, self).__init__()
         self.base_model = base_model
 
@@ -85,7 +93,9 @@ class Encoder(nn.Module):
 
 
 class GRACE(torch.nn.Module):
-    def __init__(self, encoder: Encoder, num_hidden: int, num_proj_hidden: int, tau: float = 0.5):
+    def __init__(
+        self, encoder: Encoder, num_hidden: int, num_proj_hidden: int, tau: float = 0.5
+    ):
         super(GRACE, self).__init__()
         self.encoder: Encoder = encoder
         self.tau: float = tau
@@ -112,7 +122,10 @@ class GRACE(torch.nn.Module):
         refl_sim = f(self.sim(z1, z1))
         between_sim = f(self.sim(z1, z2))
 
-        return -torch.log(between_sim.diag() / (refl_sim.sum(1) + between_sim.sum(1) - refl_sim.diag()))
+        return -torch.log(
+            between_sim.diag()
+            / (refl_sim.sum(1) + between_sim.sum(1) - refl_sim.diag())
+        )
 
     def batched_semi_loss(self, z1: torch.Tensor, z2: torch.Tensor, batch_size: int):
         # Space complexity: O(BN) (semi_loss: O(N^2))
@@ -124,17 +137,30 @@ class GRACE(torch.nn.Module):
         losses = []
 
         for i in range(num_batches):
-            mask = indices[i * batch_size:(i + 1) * batch_size]
+            mask = indices[i * batch_size : (i + 1) * batch_size]
             refl_sim = f(self.sim(z1[mask], z1))  # [B, N]
             between_sim = f(self.sim(z1[mask], z2))  # [B, N]
 
-            losses.append(-torch.log(between_sim[:, i * batch_size:(i + 1) * batch_size].diag()
-                                     / (refl_sim.sum(1) + between_sim.sum(1)
-                                        - refl_sim[:, i * batch_size:(i + 1) * batch_size].diag())))
+            losses.append(
+                -torch.log(
+                    between_sim[:, i * batch_size : (i + 1) * batch_size].diag()
+                    / (
+                        refl_sim.sum(1)
+                        + between_sim.sum(1)
+                        - refl_sim[:, i * batch_size : (i + 1) * batch_size].diag()
+                    )
+                )
+            )
 
         return torch.cat(losses)
 
-    def loss(self, z1: torch.Tensor, z2: torch.Tensor, mean: bool = True, batch_size: Optional[int] = None):
+    def loss(
+        self,
+        z1: torch.Tensor,
+        z2: torch.Tensor,
+        mean: bool = True,
+        batch_size: Optional[int] = None,
+    ):
         h1 = self.projection(z1)
         h2 = self.projection(z2)
 
@@ -150,14 +176,16 @@ class GRACE(torch.nn.Module):
 
         return ret
 
-def glorot(tensor):#inits.py中
+
+def glorot(tensor):  # inits.py中
     if tensor is not None:
         stdv = math.sqrt(6.0 / (tensor.size(-2) + tensor.size(-1)))
-        tensor.data.uniform_(-stdv, stdv)#将tensor的值设置为-stdv, stdv之间
+        tensor.data.uniform_(-stdv, stdv)  # 将tensor的值设置为-stdv, stdv之间
+
+
 def zeros(tensor):
     if tensor is not None:
         tensor.data.fill_(0)
-
 
 
 class NewGConv(MessagePassing):
@@ -193,70 +221,84 @@ class NewGConv(MessagePassing):
             :class:`torch_geometric.nn.conv.MessagePassing`.
     """
 
-    def __init__(self, in_channels, out_channels, improved=False, cached=False,
-                 bias=True, normalize=True, **kwargs):
-        super(NewGConv, self).__init__(aggr='add', **kwargs)
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        improved=False,
+        cached=False,
+        bias=True,
+        normalize=True,
+        **kwargs
+    ):
+        super(NewGConv, self).__init__(aggr="add", **kwargs)
 
-        self.in_channels = in_channels#输入通道数，也就是X的shape[1]
-        self.out_channels = out_channels#输出通道数
-        self.improved = improved#$设置为true时A尖等于A+2I
-      
-        self.cached = cached#If set to True, the layer will cache the computation of D^−1/2A^D^−1/2 on first execution, and will use the cached version for further executions. This parameter should only be set to True in transductive learning scenarios. (default: False)
-        self.normalize = normalize#是否添加自环并应用对称归一化。
+        self.in_channels = in_channels  # 输入通道数，也就是X的shape[1]
+        self.out_channels = out_channels  # 输出通道数
+        self.improved = improved  # $设置为true时A尖等于A+2I
+
+        self.cached = cached  # If set to True, the layer will cache the computation of D^−1/2A^D^−1/2 on first execution, and will use the cached version for further executions. This parameter should only be set to True in transductive learning scenarios. (default: False)
+        self.normalize = normalize  # 是否添加自环并应用对称归一化。
 
         self.weight = Parameter(torch.Tensor(in_channels, out_channels))
 
-        if bias:#如果设置为False，则该层将不会学习加法偏差
+        if bias:  # 如果设置为False，则该层将不会学习加法偏差
             self.bias = Parameter(torch.Tensor(out_channels))
         else:
-            self.register_parameter('bias', None)
+            self.register_parameter("bias", None)
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        glorot(self.weight)#glorot函数下面有写，初始化weight矩阵
-        zeros(self.bias)#zeros函数下面有写，初始化偏置矩阵
+        glorot(self.weight)  # glorot函数下面有写，初始化weight矩阵
+        zeros(self.bias)  # zeros函数下面有写，初始化偏置矩阵
         self.cached_result = None
         self.cached_num_edges = None
 
-
     @staticmethod
-    def norm(edge_index, num_nodes, edge_weight=None, improved=False,
-             dtype=None):
+    def norm(edge_index, num_nodes, edge_weight=None, improved=False, dtype=None):
         if edge_weight is None:
-            edge_weight = torch.ones((edge_index.size(1), ), dtype=dtype,
-                                     device=edge_index.device)
+            edge_weight = torch.ones(
+                (edge_index.size(1),), dtype=dtype, device=edge_index.device
+            )
 
         fill_value = 1 if not improved else 2
         edge_index, edge_weight = add_remaining_self_loops(
-            edge_index, edge_weight, fill_value, num_nodes)
+            edge_index, edge_weight, fill_value, num_nodes
+        )
 
         row, col = edge_index
         deg = scatter_add(edge_weight, row, dim=0, dim_size=num_nodes)
         deg_inv_sqrt = deg.pow(-0.5)
-        deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
+        deg_inv_sqrt[deg_inv_sqrt == float("inf")] = 0
         edge_weight = deg_inv_sqrt[row] * edge_weight * deg_inv_sqrt[col]
 
         return edge_index, edge_weight
 
-
     def forward(self, x, edge_index, c, edge_weight=None):
         """"""
-        x = torch.matmul(x, self.weight)#将x与权重矩阵相乘
-    
+        x = torch.matmul(x, self.weight)  # 将x与权重矩阵相乘
+
         if self.cached and self.cached_result is not None:
             if edge_index.size(1) != self.cached_num_edges:
                 raise RuntimeError(
-                    'Cached {} number of edges, but found {}. Please '
-                    'disable the caching behavior of this layer by removing '
-                    'the `cached=True` argument in its constructor.'.format(
-                        self.cached_num_edges, edge_index.size(1)))
+                    "Cached {} number of edges, but found {}. Please "
+                    "disable the caching behavior of this layer by removing "
+                    "the `cached=True` argument in its constructor.".format(
+                        self.cached_num_edges, edge_index.size(1)
+                    )
+                )
 
         if not self.cached or self.cached_result is None:
             self.cached_num_edges = edge_index.size(1)
             if self.normalize:
-                edge_index, norm = self.norm(edge_index, x.size(
-                    self.node_dim), edge_weight, self.improved, x.dtype)
+                edge_index, norm = self.norm(
+                    edge_index,
+                    x.size(self.node_dim),
+                    edge_weight,
+                    self.improved,
+                    x.dtype,
+                )
             else:
                 norm = edge_weight
             self.cached_result = edge_index, norm
@@ -269,7 +311,6 @@ class NewGConv(MessagePassing):
             x = 0.5 * x
         return x
 
-
     def message(self, x_j, norm):
         return norm.view(-1, 1) * x_j if norm is not None else x_j
 
@@ -279,14 +320,25 @@ class NewGConv(MessagePassing):
         return aggr_out
 
     def __repr__(self):
-        return '{}({}, {})'.format(self.__class__.__name__, self.in_channels,
-                                   self.out_channels)
+        return "{}({}, {})".format(
+            self.__class__.__name__, self.in_channels, self.out_channels
+        )
+
 
 class NewEncoder(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, activation, base_model=GATConv, k: int = 2, skip=False):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        activation,
+        base_model=GATConv,
+        k: int = 2,
+        skip=False,
+    ):
+        # skip meaning?
         super(NewEncoder, self).__init__()
         self.base_model = base_model
-        #self.adj = adj
+        # self.adj = adj
         assert k >= 1
         self.k = k
         self.skip = skip
@@ -294,6 +346,7 @@ class NewEncoder(nn.Module):
         hi = 1
         if k == 1:
             self.conv = [base_model(in_channels, out_channels).jittable()]
+            # jittable：将conv转化为messagePassing可以接受的模式
             self.conv = nn.ModuleList(self.conv)
             self.activation = activation
         elif not self.skip:
@@ -312,42 +365,56 @@ class NewEncoder(nn.Module):
             self.conv = nn.ModuleList(self.conv)
 
             self.activation = activation
+            # self.skip是在干什么
 
-    def forward(self, x: torch.Tensor, edge_index: torch.Tensor, R = [1,2], final = False):
+    def forward(self, x: torch.Tensor, edge_index: torch.Tensor, R=[1, 2], final=False):
         if final == False:
             K2 = R[1]
             K1 = R[0]
-        if final: 
+        if final:
             K1 = 2
             K2 = 2
         feat = x
         x = self.activation(self.conv[0](feat, edge_index, K1))
         x = self.conv[1](x, edge_index, K2)
-        
+
         return x
+        # 这又是在干什么？
+
 
 class NewGRACE(torch.nn.Module):
-    def __init__(self, encoder: NewEncoder, Adj, num_hidden: int, num_proj_hidden: int, tau: float = 0.5):
+    def __init__(
+        self,
+        encoder: NewEncoder,
+        Adj,
+        num_hidden: int,
+        num_proj_hidden: int,
+        tau: float = 0.5,
+    ):
         super(NewGRACE, self).__init__()
         self.encoder: NewEncoder = encoder
-        #self.encoder2: Encoder = encoder2
-        #self.BCE = torch.nn.BCELoss()
+        # self.encoder2: Encoder = encoder2
+        # self.BCE = torch.nn.BCELoss()
         self.tau: float = tau
         self.adj = Adj
-        #self.adj = adj
-        #self.A = A
-        #self.norm = (A.shape[0] * A.shape[0]) / (float((A.shape[0] * A.shape[0] - torch.sum(A))) * 2)
+        # self.adj = adj
+        # self.A = A
+        # self.norm = (A.shape[0] * A.shape[0]) / (float((A.shape[0] * A.shape[0] - torch.sum(A))) * 2)
         self.fc1 = torch.nn.Linear(num_hidden, num_proj_hidden)
         self.fc2 = torch.nn.Linear(num_proj_hidden, num_hidden)
 
         self.num_hidden = num_hidden
 
-    def forward(self, x: torch.Tensor, edge_index: torch.Tensor, R = [1,2], final = False) -> torch.Tensor:
-        return self.encoder(x, edge_index, R, final)#, self.encoder2(x, edge_index)
+    def forward(
+        self, x: torch.Tensor, edge_index: torch.Tensor, R=[1, 2], final=False
+    ) -> torch.Tensor:
+        return self.encoder(x, edge_index, R, final)
+
+        # , self.encoder2(x, edge_index)
 
     def projection(self, z: torch.Tensor) -> torch.Tensor:
         z = F.elu(self.fc1(z))
-        #z = self.fc1(z)
+        # z = self.fc1(z)
         return self.fc2(z)
 
     def sim(self, z1: torch.Tensor, z2: torch.Tensor):
@@ -363,26 +430,31 @@ class NewGRACE(torch.nn.Module):
         ret = -torch.log(between_sim.diag() / ref_sim.sum(1))
         ret = ret.mean()
         return ret
+
     """   
     def GAELoss(self, z: torch.Tensor):
         act = nn.Sigmoid()
         return self.norm * self.BCE(act(torch.mm(z, z.t())), self.A)
     """
 
-    def semi_loss(self, z1: torch.Tensor, z2: torch.Tensor, k = 0, r=0.9):
+    def semi_loss(self, z1: torch.Tensor, z2: torch.Tensor, k=0, r=0.9):
 
         f = lambda x: torch.exp(x / self.tau)
-        
 
         z1 = F.normalize(z1)
         z2 = F.normalize(z2)
-        
-        between_sim = f(torch.mm(z1, z2.t()))
-        
-        return -torch.log(between_sim.diag() / between_sim.sum(1))
-    
 
-    def loss(self, z1: torch.Tensor, z2: torch.Tensor, mean: bool = True, batch_size: Optional[int] = None):
+        between_sim = f(torch.mm(z1, z2.t()))
+
+        return -torch.log(between_sim.diag() / between_sim.sum(1))
+
+    def loss(
+        self,
+        z1: torch.Tensor,
+        z2: torch.Tensor,
+        mean: bool = True,
+        batch_size: Optional[int] = None,
+    ):
         h1 = z1
         h2 = z2
         if batch_size is None:
@@ -391,9 +463,9 @@ class NewGRACE(torch.nn.Module):
             l = self.semi_loss(h1, h2)
 
         ret = l
-        #ret = l1
+        # ret = l1
         ret = ret.mean() if mean else ret.sum()
-        #ret = ret# + 0.1 * self.GAELoss(z1) + 0.1 * self.GAELoss(z2)
+        # ret = ret# + 0.1 * self.GAELoss(z1) + 0.1 * self.GAELoss(z2)
         return ret
 
 
